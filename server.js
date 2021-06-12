759a0239eac6cd78cc05e827da254ea1f5eef6d5
chery-pynya <nikita.cherepnya@gmail.com>
2021-06-12 22:28:33 +0300

    heroku cors

--- server.js
+++ server.js
@@ -135,4 +135,5 @@ app.use(async(ctx, next)  => {
   }
 })
 
-const server = http.createServer(app.callback()).listen(7070);
+const port = process.env.PORT || 7070;
+const server = http.createServer(app.callback()).listen(port)
